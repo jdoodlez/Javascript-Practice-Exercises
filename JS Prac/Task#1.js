af78eeb68ab2bb2df92444ecdfe2f55ede81304b
Jhon Dale C. Sanchez <jhondalecsanchez@iskolarngbayan.pup.edu.ph>
2025-07-09 16:57:46 +0800

    Task #1 - Variables and Data Types

---
+++ JS Prac/Task#1.js
@@ -0,0 +1,29 @@
+// Practice Set 1 — Variables & Data Types
+
+// Main Exercise: Declare a variable called name and assign your name to it. Declare a variable age and assign your age. Print both using "console.log".
+
+//Extra Practice:
+
+/*  
+
+1. Declare a constant PI with the value 3.14 and log it.
+2. Try reassigning the PI value and see what happens.
+3. Declare three different types of variables: a number, a string, and a boolean. Log their types using typeof. */
+
+
+let name = "JD";
+let age = "20";
+
+console.log("My name is ${name} and I am ${age} years old.");
+
+const PI = 3.14;
+
+console.log(PI);
+
+let num = 24;
+let string = "Hilo gudmorning";
+let boolean = true;
+
+console.log(typeof num); 
+console.log(typeof string); 
+console.log(typeof boolean); 
